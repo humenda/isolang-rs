21ac7d72b8487f3cade5cf7231b75edb2fdcd72a
Sebastian Humenda <shumenda@gmx.de>
2017-03-24 19:38:21 +0100

    Generate tables for 639-1/3 codes and an overview table

--- build.rs
+++ build.rs
@@ -1,6 +1,5 @@
 extern crate phf_codegen;
 
-use std::collections::HashSet;
 use std::env;
 use std::iter::FromIterator;
 use std::fs::File;
@@ -8,16 +7,21 @@ use std::io::{BufReader, BufRead, BufWriter, Write};
 use std::path::Path;
 
 // Taken from http://www-01.sil.org/iso639-3/download.asp
-
 static ISO_TABLE_PATH: &'static str = "iso-639-3.tab";
-// title first character
+
+/// This contains (639-3, 639-1, English name, comment)
+type LangCodes = Vec<(String, Option<String>, String, Option<String>)>;
+
+
+/// convert first character to upper case
 fn title(s: &str) -> String {
     let mut v: Vec<char> = s.chars().collect();
     v[0] = v[0].to_uppercase().nth(0).unwrap();
     v.into_iter().collect::<String>()
 }
 
-fn read_iso_table() -> Vec<(String, String, String)> {
+/// parse ISO 6639-(3,1) table
+fn read_iso_table() -> LangCodes {
     let r = BufReader::new(File::open(ISO_TABLE_PATH).expect(r"\
         Couldn't read iso-639-3.tab. Make sure that his operation is run from \
         the crate source root and that this file actually exists."));
@@ -25,9 +29,57 @@ fn read_iso_table() -> Vec<(String, String, String)> {
         let line = line.expect("Couldn't read from ISO 639 table, please check \
             that the file exists and is readable");
         let cols = line.split("\t").collect::<Vec<&str>>();
-        (cols[0].into(), cols[3].into(), cols[6].into())
+        let two_letter: Option<String> = match cols[3].len() {
+            2 => Some(cols[3].into()),
+            _ => None
+        };
+        // split language string into name and comment, if required
+        match cols[6].contains("(") {
+            false => (cols[0].into(), two_letter, cols[6].into(), None),
+            true => match cols[6].split(" (").collect::<Vec<&str>>() {
+                ref m if m.len() != 2  => (cols[0].into(), two_letter, cols[6].into(), None),
+                m => (cols[0].into(), two_letter, m[0].into(), Some(m[1].into())),
+            }
+        }
     }).collect()
 }
+
+/// write static array with (639-3, 639-1, English word, comment) entries
+fn write_overview_table(file: &mut BufWriter<File>, codes: &LangCodes) {
+    writeln!(file, "static OVERVIEW: [(&'static str, Option<&'static str>, \
+            &'static str, Option<&'static str>); {}] = [", codes.len()).unwrap();
+    for line in codes.iter() {
+        writeln!(file, "    {:?},", line).unwrap();
+    }
+    write!(file, "];\n").unwrap();
+}
+
+
+/// Write a mapping of codes from 639-1 -> Language::`639-3`
+fn write_two_letter_to_enum(file: &mut BufWriter<File>, codes: &LangCodes) {
+    write!(file, "static TWO_TO_THREE: phf::Map<&'static str, Language> = ").unwrap();
+    let mut map = phf_codegen::Map::new();
+    for &(ref id, ref two_letter, _, _) in codes.iter() {
+        if let &Some(ref two_letter) = two_letter {
+            map.entry(two_letter.clone(), &format!("Language::{}", title(id)));
+        }
+    }
+    map.build(file).unwrap();
+    writeln!(file, ";").unwrap();
+}
+
+/// Write a mapping of codes from 639-3 -> Language::`639-3`
+fn write_three_letter_to_enum(file: &mut BufWriter<File>, codes: &LangCodes) {
+    write!(file, "static THREE_TO_THREE: phf::Map<&'static str, Language> = ").unwrap();
+    let mut map = phf_codegen::Map::new();
+    for &(ref id, _, _, _) in codes.iter() {
+        map.entry(id.clone(), &format!("Language::{}", title(id)));
+    }
+    map.build(file).unwrap();
+    writeln!(file, ";").unwrap();
+}
+
+
 
 fn main() {
     let path = Path::new(&env::var("OUT_DIR").unwrap()).join("isotable.rs");
@@ -37,32 +89,21 @@ fn main() {
         let mut file = BufWriter::new(File::create(&path).expect(r"Couldn't \
                 write to output directory, compilation impossible"));
 
-        // write static array with (639-3, 639-1, English word) entries
-        write!(&mut file, "pub enum Language {{");
-        for (num, &(ref id, _, _)) in codes.iter().enumerate() {
+        // write overview table with all data
+        write_overview_table(&mut file, &codes);
+
+        // write enum with 639-3 codes (num is the index into the overview table)
+        write!(&mut file, "\npub enum Language {{\n").unwrap();
+        for (num, &(ref id, _, _, _)) in codes.iter().enumerate() {
             writeln!(&mut file, "    {} = {},", title(id), num).unwrap();
         }
         writeln!(&mut file, "}}\n\n").unwrap();
 
-        // write map with English language name -> list of 639-3 codes
-        let mut map = phf_codegen::Map::new();
-        write!(&mut file, "static ENGLISH2IDX: phf::Map<&'static str, usize> = ").unwrap();
-        for (num, &(_, _, ref engl)) in codes.iter().enumerate() {
-            map.entry(engl.clone(), &num.to_string());
-        }
-        map.build(&mut file).unwrap();
-        write!(&mut file, ";\n").unwrap();
+        // write map 639-1 -> enum mapping
+        write_two_letter_to_enum(&mut file, &codes);
 
-
-        /* ToDo: solve issue of doubled ID's
-        // write 639-1 to 639-3 mapping
-        write!(&mut file, "static TWO_TO_THREE: phf::Map<&'static str, &'static str> = ").unwrap();
-        for &(ref id, ref short, ref english) in codes.iter() {
-            map.entry(short.clone(), id.clone().as_str());
-        }
-        map.build(&mut file).unwrap();
-        write!(&mut file, ";\n").unwrap();
-        */
+        // write map 639-3 -> enum mapping
+        write_three_letter_to_enum(&mut file, &codes);
     }
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ extern crate phf;
 include!(concat!(env!("OUT_DIR"), "/isotable.rs"));
 
 
+
 #[cfg(test)]
 mod tests {
     #[test]
